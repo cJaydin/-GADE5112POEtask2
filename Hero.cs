50d8c1c03c561ce9009cfb6f950d7d7e1b9117d8
cJaydin <Jaydinc1219@gmail.com>
2020-11-25 12:38:54 +0200

    Q.2.6.

---
+++ Hero.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace GADE5112POE
+{
+    class Hero : Character
+    {
+        public Hero(int x, int y, int hp) : base(hp, 2, 10, x, y) { }
+        public override Movement ReturnMove(Movement move = Movement.Idle)
+        {
+            if (Vision[(int)move].GetType()!=typeof(EmptyTile))
+            {
+                return Movement.Idle;
+            }
+
+            switch (move)
+            {
+                case (Movement.Up): Y--; break;
+                case (Movement.Down): Y++; break;
+                case (Movement.Left): X--; break;
+                case (Movement.Right): X++; break;
+            }
+
+            return move;
+        }
+
+        public override string ToString()
+        {
+            return "Player Stats: " + Environment.NewLine + "HP: " + Hp + "/" + MaxHp + Environment.NewLine + "Damage: " + Damage + Environment.NewLine + "[" + X + "," + Y + "]";
+        }
+    }
+}
