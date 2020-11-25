ae261f9f284810e743b0dddc2caadd3b1044e65c
cJaydin <Jaydinc1219@gmail.com>
2020-11-25 10:17:55 +0200

    Added Tile.cs

---
+++ Tile.cs
@@ -0,0 +1,20 @@
+﻿namespace GADE5112POE
+{
+    abstract class Tile
+    {
+        public enum TileType { Hero, Enemy, Gold, Weapon }
+
+        protected int x;
+        protected int y;
+
+        public int X { get => x; set => x = value; }
+        public int Y { get => y; set => y = value; }
+
+
+        public Tile(int x, int y)
+        {
+            X = x;
+            Y = y;
+        }
+    }
+}
